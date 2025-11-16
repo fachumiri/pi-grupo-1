99f35c1ec168dd4146b9023f6011e1caecf273e5
fachumiri <fmiri635@gmail.com>
2025-11-16 11:01:57 -0300

    Modifique el login, falta enviar el registro

--- js/login.js
+++ js/login.js
@@ -3,7 +3,6 @@ let inputemail = document.querySelector(".inputlogin")
 let inputcontra = document.querySelector (".contraseña")
 
 formulariolog.addEventListener("submit",function (e) { 
-    e.preventDefault() // prevenir un comportamiento por defecto
     //recupero los valores de los campos del form 
    let email = inputemail.value;
    let password = inputcontra.value;
@@ -13,10 +12,13 @@ formulariolog.addEventListener("submit",function (e) {
     e.preventDefault()
     alert("Debe completar los campos");
    } else if (email === ""){
+     e.preventDefault() 
     alert("Debe completar el campo Email")
    } else if (password === ""){
+    e.preventDefault()
     alert("Debe completar el campo Contraseña")
    }else if (password.length > 0 && password.length < 6){
+    e.preventDefault()
     alert("La contraseña debe tener al menos 6 caracteres")
    }else{
 
