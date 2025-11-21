cd40ffb7acdbcb6be2395ea3ed9d9c2bc2b6d344
mdisanto-gif <mdisanto@udesa.edu.ar>
2025-11-21 10:21:09 -0300

    hice la seccion de resenas

--- js/detalle.js
+++ js/detalle.js
@@ -1,21 +1,24 @@
 // URL de los detalles
-let queryString = location.search; 
+let queryString = location.search;
 let qs = new URLSearchParams(queryString);
-let producto = qs.get("id");  
+let producto = qs.get("id");
 
 console.log("producto seleccionado", producto);
 
 let urlId = `https://dummyjson.com/products/${producto}`;
 
+let contenedor = document.querySelector(".rev");
+let detalle = document.querySelector(".det");
+
 // Sección 
 fetch(urlId)
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
 
-        let detalle = document.querySelector(".det");
+
 
         let detalleproducto = `
              <div class="detalle-producto">
@@ -24,7 +27,7 @@ fetch(urlId)
                 <p class="productos.detalle"><strong>Marca:</strong> ${data.brand}</p>
                 <p class="productos.detalle"><strong>Descripción:</strong> ${data.description}</p>
                 <p class="productos.detalle"><strong>Precio:</strong>${data.price}</p>
-                <p class="productos.detalle"><strong>Categoría:</strong> ${data.category}</p>
+                <p class="productos.detalle"><strong>Categoría:</strong> <a href="./category.html?cat=${data.category}">${data.category}</a></p>
                 <p class="productos.detalle"><strong>Stock:</strong> ${data.stock} unidades disponibles</p>
                 <p class="productos.detalle"><strong>Tags:</strong> ${data.tags}</p>
             </div>
@@ -34,60 +37,25 @@ fetch(urlId)
 
         detalle.innerHTML += detalleproducto;
 
-        let resenas = document.querySelector(".rev")
+        //comentarios
+        let comentarios = data.reviews;
+        console.log(comentarios);
 
-    
-    //falta reseña
+        let contenido = "";
 
-    // 1. Recuperar ID del producto desde el QS
-let qs = location.search;
-let qsObj = new URLSearchParams(qs);
-let productId = qsObj.get("id");
+        for (let i = 0; i < comentarios.length; i++) {
+            let review = comentarios[i];
 
-console.log("Producto ID:", productId);
+            contenido += `
+              <div class="review">
+                <p><strong> ${review.reviewerName} — ${review.date} </strong></p>
+                <p>${review.rating}/5</p>
+                <p>${review.comment}</p>
+              </div>
+            <hr>    
+        `;
 
-// 2. URL de reviews del producto
-let urlReviews = "⁠https://dummyjson.com/comments/post/${productId}"
-
-// 3. Capturar contenedor
-let contenedorReviews = document.querySelector(".lista-reviews");
-
-// 4. Fetch para reviews
-fetch(urlReviews)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log("Reviews:", data);
-
-        let comentarios = data.comments;
-        let html = "";
-
-        if(comentarios.length === 0){
-            html = "<p>No hay reviews para este producto.</p>";
         }
+        detalle.innerHTML += contenido
 
-        for(let i = 0; i < comentarios.length; i++){
-            let rev = comentarios[i];
-
-            html += `
-                <article class="review">
-                    <p>Rating: ${rev.rating}</p>
-                    <p>Comentario: ${rev.body}</p>
-                    <p>Fecha: ${rev.date}</p>
-                    <p>Usuario: ${rev.user.username}</p>
-                </article>
-            
-            `;
-        }
-
-        contenedorReviews.innerHTML = html;
     })
-    .catch(function(error){
-        console.log("Error en reviews:", error);
-    });
-       
-    })
-    .catch(function(error){
-        console.log("Error categoría 1: " + error);
-    });
