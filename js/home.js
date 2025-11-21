0ab2ee9cc0bfc6db21a3c04544a17e8722e7f6e7
mdisanto-gif <mdisanto@udesa.edu.ar>
2025-11-21 08:36:51 -0300

    agregue criterio de busqueda

--- js/home.js
+++ js/home.js
@@ -72,6 +72,32 @@ fetch(urlCat2)
         console.log("Error categoría 2: " + error);
     })
 
+// busqueda
+
+const form = document.querySelector('.barranav form');
+const input = document.querySelector('input[name="busqueda"]');
+
+form.addEventListener('submit', function (event) {
+    const valor = input.value.trim();
+
+
+    if (valor === "") {
+        event.preventDefault(); 
+        alert("El campo de búsqueda no puede estar vacío.");
+        return;
+    }
+
+    if (valor.length < 3) {
+        event.preventDefault();
+        alert("La búsqueda debe tener al menos 3 caracteres.");
+        return;
+    }
+
+    
+});
+
+
+
 
 
 
