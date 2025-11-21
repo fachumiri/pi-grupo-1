2fef136ccb45bcd7b0b16f1a860c118fd0b570fe
mdisanto-gif <mdisanto@udesa.edu.ar>
2025-11-21 08:34:16 -0300

    complete detalle, falta resena

---
+++ js/detalle.js
@@ -0,0 +1,45 @@
+// URL de los detalles
+let queryString = location.search; 
+let qs = new URLSearchParams(queryString);
+let producto = qs.get("id");  
+
+console.log("producto seleccionado", producto);
+
+let urlId = `https://dummyjson.com/products/${producto}`;
+
+// Sección 
+fetch(urlId)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let detalle = document.querySelector(".det");
+
+        let detalleproducto = `
+             <div class="detalle-producto">
+                <img class="producto.detalle img" src="${data.images[0]}" alt="${data.title}">
+                <h2 class="h2.detalle">${data.title}</h2>
+                <p class="productos.detalle"><strong>Marca:</strong> ${data.brand}</p>
+                <p class="productos.detalle"><strong>Descripción:</strong> ${data.description}</p>
+                <p class="productos.detalle"><strong>Precio:</strong>${data.price}</p>
+                <p class="productos.detalle"><strong>Categoría:</strong> ${data.category}</p>
+                <p class="productos.detalle"><strong>Stock:</strong> ${data.stock} unidades disponibles</p>
+                <p class="productos.detalle"><strong>Tags:</strong> ${data.tags}</p>
+            </div>
+            
+        
+        `
+
+        detalle.innerHTML += detalleproducto;
+
+        let resenas = document.querySelector(".rev")
+
+    
+    //falta reseña
+       
+    })
+    .catch(function(error){
+        console.log("Error categoría 1: " + error);
+    });
